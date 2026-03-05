f89cc32010621c23df15679784ca357e25fe57d3
H1101241014-Naomy <h1101241014@student.untan.ac.id>
2026-03-05 11:21:35 +0700

    Praktikum 2

--- src/validation.ts
+++ src/validation.ts
@@ -1,8 +1,11 @@
 import { Elysia, t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 
+
 const app = new Elysia()
-  .use(openapi())
+  .use(openapi({
+})
+)
   .post("/request",
     ({ body }) => {
       return {
@@ -18,5 +21,29 @@ const app = new Elysia()
       })
     }
   )
- .listen(3000);
+  .get(
+    "/products/:id",
+    ({ params, query }) => {
+      return {
+        id: params.id,
+       sort: query.sort
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Numeric()
+      }),
+      query: t.Object({
+        sort: t.Union([
+          t.Literal("asc"),
+          t.Literal("desc")
+        ])
+      }),
+    response: t.Object({
+      id: t.Number(),
+      sort: t.String()
+    })
+  }
+)
+  app.listen(3000);
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
